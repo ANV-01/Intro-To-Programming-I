f5b4de6d36d7f1f7a8b2b53ac6c10178683c2877
ANV-01 <92701486+ANV-01@users.noreply.github.com>
2021-10-25 03:15:04 -0700

    Add files via upload

---
+++ HW02P03.java
@@ -0,0 +1,9 @@
+public class HW02P03 {
+    public static void main(String[] args){
+        System.out.println(
+                "     J        A    V     V\n" +
+                "     J       A A    V   V\n" +
+                "J    J      AAAAA    V V\n" +
+                " J  J      A     A    V");
+    }
+}
