224b9fd80148ec3b926f19b3b1926e85ef0135e9
ANV-01 <92701486+ANV-01@users.noreply.github.com>
2021-10-25 03:14:56 -0700

    Add files via upload

---
+++ HW02P02.java
@@ -0,0 +1,8 @@
+public class HW02P02 {
+    public static void main(String[] args){
+        System.out.println("I come with no wrapping or pretty pink bows.\n" +
+                "I am who I am, from my head to my toes.\n" +
+                "I tend to get loud when speaking my mind.\n" +
+                "Even a little crazy some of the time.\n");
+    }
+}
